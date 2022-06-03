b7fc7a1a110e3d60c2c3e768f8773b0ae84902f2
Madhumida <smadhumida@gmail.com>
2022-06-02 22:00:15 -0700

    Add Input component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
+import Index from "../pages/Index"
 
-function Main(props) {
+function Main() {
     const [bookmark, setBookmar] = useState(null)
     const URL = "https://vanilla-ice-bookmark-backend.herokuapp.com/bookmarks"
 
@@ -12,9 +13,31 @@ function Main(props) {
     }
 
 
+    const createBookmark = async (bookmark) => {
+        console.log(bookmark)
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(bookmark),
+
+        })
+
+        // update list of people
+        getBookmarks();
+    }
 
     useEffect(() => {
         getBookmarks()
     }, [])
+
+    return (
+        < main >
+            <Routes>
+                <Route path="/" element={<Index bookmark={bookmark} createBookmark={createBookmark} />}></Route>
+            </Routes>
+        </main >
+    )
 }
 export default Main
