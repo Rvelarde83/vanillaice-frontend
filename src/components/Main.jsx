f15019a0afad184cc272ddbdcda2c9822f3c99c4
Ramon Velarde <rvelarde83@hotmail.com>
2022-06-04 10:32:16 -0700

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 
 function Main(props) {
-    const [bookmark, setBookmar] = useState(null)
+    const [bookmark, setBookmark] = useState(null)
     const URL = "https://vanilla-ice-bookmark-backend.herokuapp.com/bookmarks"
 
     const getBookmarks = async () => {
         const response = await fetch(URL)
         const data = await response.json()
-        setBookmar(data)
+        setBookmark(data)
     }
 
 
